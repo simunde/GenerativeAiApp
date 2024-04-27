60d1ca88ef0b5df305ff407ea2f54a9b280bb740
simunde <sidmundece17@gmail.com>
2024-04-27 11:58:28 -0600

    Create a AI App by integrating Gemini

---
+++ app/src/main/java/com/msid/generativeaiapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.msid.generativeaiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.ai.client.generativeai.GenerativeModel
+import com.msid.generativeaiapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.runBlocking
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnSubmit.setOnClickListener {
+
+            val userPrompt = binding.edtUserPrompt.text.toString()
+            val generativeModel = GenerativeModel(
+                // Use a model that's applicable for your use case (see "Implement basic use cases" below)
+                modelName = "gemini-pro",
+                // Access your API key as a Build Configuration variable (see "Set up your API key" above)
+                apiKey = "your api key"
+            )
+
+            runBlocking {
+                val response = generativeModel.generateContent(userPrompt)
+                binding.tvResult.text = response.text
+            }
+
+
+        }
+    }
+}
+
+
+//AIzaSyDr2sjL2ehPnb7Hj0XM9DriAjo5VYe8oQQ
